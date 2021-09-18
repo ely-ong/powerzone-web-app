2a70fdae43845ecf87e8c1a5c49b10e9245ee1d0
LanderPeterCua <80734810+LanderPeterCua@users.noreply.github.com>
2021-09-18 15:47:13 +0800

    Front-end validation for add transaction
    Button disabled when:
    - no product is selected
    - product is selected but missing details

--- PowerzoneWebApp/public/js/addTransaction.js
+++ PowerzoneWebApp/public/js/addTransaction.js
@@ -82,6 +82,23 @@ $(document).ready(function() {
         $('#input_addTransactSellingPriceKerosene').val(parseFloat(qty).toFixed(2));
     })
 
+    // Helper Functions
+
+    // helper function that disables submit button and adds error message
+    function disableSubmit(error) {
+        console.log("disable" + error);
+        $('#btn_addTransact').prop('disabled', true);
+        $('#errorBottom').text(error);
+        $('#errorBottom').css('color', '#ab4642');
+    }
+
+    // helper function that enables submit button and removes error message
+    function enableSubmit() {
+        $('#btn_addTransact').prop('disabled', false);
+        $('#errorBottom').text('');
+        $('#errorBottom').css('color', 'transparent');
+    }
+
     // Front-End Validation
 
     // checks if at least one product was selected in the transaction
@@ -94,21 +111,126 @@ $(document).ready(function() {
         var checkboxKerosene = $('#add_checkbox_kerosene').is(":checked");
 
         if(!checkboxDiesel && !checkboxGasoline && !checkboxPremium95 && !checkboxPremium97 && !checkboxKerosene){
-            $('#btn_addTransact').prop('disabled', true);
-            $('#errorBottom').text('Select at least one product to purchase.');
-            $('#errorBottom').css('color', '#ab4642');
+            disableSubmit('Select at least one product to purchase.');
         }
         else{
-            $('#btn_addTransact').prop('disabled', false);
-            $('#errorBottom').text('');
+            var isComplete = true;
+
+            if(checkboxDiesel){
+                var quantityDiesel = $('#input_addTransactQuantityDiesel').val();
+                var priceDiesel = $('#input_addTransactSellingPriceDiesel').val();
+
+                if(quantityDiesel == '' || priceDiesel == ''){
+                    isComplete = false;
+                }
+            }
+
+            if(checkboxGasoline){
+                var quantityGasoline = $('#input_addTransactQuantityGasoline').val();
+                var priceGasoline = $('#input_addTransactSellingPriceGasoline').val();
+
+                if(quantityGasoline == '' || priceGasoline == ''){
+                    isComplete = false;
+                }
+            }
+
+            if(checkboxPremium95){
+                var quantityPremium95 = $('#input_addTransactQuantityPremium95').val();
+                var pricePremium95 = $('#input_addTransactSellingPricePremium95').val();
+
+                if(quantityPremium95 == '' || pricePremium95 == ''){
+                    isComplete = false;
+                }
+            }
+
+            if(checkboxPremium97){
+                var quantityPremium97 = $('#input_addTransactQuantityPremium97').val();
+                var pricePremium97 = $('#input_addTransactSellingPricePremium97').val();
+
+                if(quantityPremium97 == '' || pricePremium97 == ''){
+                    isComplete = false;
+                }
+            }
+
+            if(checkboxKerosene){
+                var quantityKerosene = $('#input_addTransactQuantityKerosene').val();
+                var priceKerosene = $('#input_addTransactSellingPriceKerosene').val();
+
+                if(quantityKerosene == '' || priceKerosene == ''){
+                    isComplete = false;
+                }
+            }
+
+            if(isComplete){
+                enableSubmit();
+            }
+            else{
+                disableSubmit('Incomplete product details above.');
+            }
         }
     })
 
-    // checks if product fields were inputted
-    // $('#btn_addTransact').click(function(){
+    $('#input_addTransactQuantityDiesel, #input_addTransactSellingPriceDiesel, #input_addTransactQuantityGasoline, #input_addTransactSellingPriceGasoline,' + 
+      '#input_addTransactQuantityPremium95, #input_addTransactSellingPricePremium95, #input_addTransactQuantityPremium97, #input_addTransactSellingPricePremium97,' +
+      '#input_addTransactQuantityKerosene, #input_addTransactSellingPriceKerosene').keyup(function(){
 
-    //     if(checkboxDiesel){
+        var checkboxDiesel = $('#add_checkbox_diesel').is(":checked");
+        var checkboxGasoline = $('#add_checkbox_gasoline').is(":checked");
+        var checkboxPremium95 = $('#add_checkbox_premium95').is(":checked");
+        var checkboxPremium97 = $('#add_checkbox_premium97').is(":checked");
+        var checkboxKerosene = $('#add_checkbox_kerosene').is(":checked");
+        var isComplete = true;
 
-    //     }
-    // })
+        if(checkboxDiesel){
+            var quantityDiesel = $('#input_addTransactQuantityDiesel').val();
+            var priceDiesel = $('#input_addTransactSellingPriceDiesel').val();
+
+            if(quantityDiesel == '' || priceDiesel == ''){
+                isComplete = false;
+            }
+        }
+
+        if(checkboxGasoline){
+            var quantityGasoline = $('#input_addTransactQuantityGasoline').val();
+            var priceGasoline = $('#input_addTransactSellingPriceGasoline').val();
+
+            if(quantityGasoline == '' || priceGasoline == ''){
+                isComplete = false;
+            }
+        }
+
+        if(checkboxPremium95){
+            var quantityPremium95 = $('#input_addTransactQuantityPremium95').val();
+            var pricePremium95 = $('#input_addTransactSellingPricePremium95').val();
+
+            if(quantityPremium95 == '' || pricePremium95 == ''){
+                isComplete = false;
+            }
+        }
+
+        if(checkboxPremium97){
+            var quantityPremium97 = $('#input_addTransactQuantityPremium97').val();
+            var pricePremium97 = $('#input_addTransactSellingPricePremium97').val();
+
+            if(quantityPremium97 == '' || pricePremium97 == ''){
+                isComplete = false;
+            }
+        }
+
+        if(checkboxKerosene){
+            var quantityKerosene = $('#input_addTransactQuantityKerosene').val();
+            var priceKerosene = $('#input_addTransactSellingPriceKerosene').val();
+
+            if(quantityKerosene == '' || priceKerosene == ''){
+                isComplete = false;
+            }
+        }
+
+        if(isComplete){
+            enableSubmit();
+        }
+        else{
+            disableSubmit('Incomplete product details above.');
+        }
+    });
 })
